96d546d5136bfcf89ae43a62c9e78819e8493458
Enzo <enzo.bondan@hotmail.com>
2025-08-18 22:22:31 -0400

    alterado arquivo de conexao banco de dados

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,10 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@db:5432/projects_db')
+    DB_USER = os.getenv('DB_USER', 'user')
+    DB_PASS = os.getenv('DB_PASS', 'password')
+    DB_NAME = os.getenv('DB_NAME', 'meubanco')
+    DB_HOST = os.getenv('DB_HOST', 'db')
+    
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
