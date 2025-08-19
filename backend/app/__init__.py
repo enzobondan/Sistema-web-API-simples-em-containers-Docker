03bbf756e004702c643fdd29d5dbcd0adbf0bebd
Enzo <enzo.bondan@hotmail.com>
2025-08-18 22:55:49 -0400

    faltou salvar arquivo xd

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('config.Config')
     
     db.init_app(app)
